3f55e5264f140fe61520c1b7e57c71533db2a65a
Alex <40299833+Plarpoon@users.noreply.github.com>
2022-03-14 22:10:01 +0100

    Add files via upload

---
+++ DoNotStarveTogether_HelheimCaves/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,16 @@
+return {
+--#Display Attack Range
+["2078243581"] = { enabled = true },
+
+--#Global Position
+["378160973"] = { enabled = true },
+
+--#Health Info
+["375859599"] = { enabled = true },
+
+--#Show Me (Origin)
+["666155465"] = { enabled = true },
+
+--#Simple Health Bar DST
+["1207269058"] = { enabled = true }
+}
